b4510fafbe1fb4cc73f1ab3fdecb591ec15cecb3
Rasa <abdurasul2004.02@gmail.com>
2022-03-16 19:04:21 +0500

    categories

---
+++ resources/views/admin/includes/sidebar.blade.php
@@ -0,0 +1,28 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<!-- Sidebar -->
+<div class="sidebar">
+  <ul class="nav nav-pills nav-sidebar flex-column pt-3" data-widget="treeview">
+
+    <li class="nav-item">
+      <a href="{{ route('admin.category.index') }}" class="nav-link">
+        <i class="nav-icon fas fa-list"></i>
+        <p>
+          Категории
+        </p>
+      </a>
+    </li>
+
+    <li class="nav-item">
+      <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-hashtag"></i>
+        <p>
+          Теги
+        </p>
+      </a>
+    </li>
+
+  </ul>
+</div>
+<!-- /.sidebar -->
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['namespace' => 'App\Http\Controllers\Main'], function(){
+    Route::get('/', 'IndexController');
 });
+
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function(){
+    Route::group(['namespace' => 'Main'], function(){
+        Route::get('/', 'IndexController');
+    });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function(){
+        Route::get('/', 'IndexController')->name('admin.category.index');
+        Route::get('/create', 'CreateController')->name('admin.category.create');
+        Route::post('/', 'StoreController')->name('admin.category.store');
+        Route::get('/{category}', 'ShowController')->name('admin.category.show');
+        Route::get('/{category}/edit', 'EditController')->name('admin.category.edit');
+        Route::patch('/{category}', 'UpdateController')->name('admin.category.update');
+        Route::delete('/{category}', 'DeleteController')->name('admin.category.delete');
+    });
+
+});
+
+Auth::routes();
